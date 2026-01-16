02137d2d1c3d01b3ee0769b4ffc366f45a8e79b0
prashantmore45 <prashantmorepm05@gmail.com>
2026-01-16 17:49:35 +0530

    chore: prepare config for deployment

--- Job-Board/frontend/src/services/api.js
+++ Job-Board/frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const BACKEND_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
+const API = axios.create({ baseURL: BACKEND_URL });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
